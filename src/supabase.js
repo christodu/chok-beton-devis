308840c75b9efd5e00f3dfc3eb433abefb969625
christodu <christopher.chokbeton@gmail.com>
2026-06-03 22:49:52 +0200

    Remove Supabase client initialization

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,1 @@
-import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://vafdxhneykqervstkkew.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZmR4aG5leWtxZXJ2c3Rra2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDgxODQsImV4cCI6MjA5NjA4NDE4NH0.Y-p9a22T94SMo4IfRjeV0CyeiNxmw5fTu1LNPzZkJho'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
